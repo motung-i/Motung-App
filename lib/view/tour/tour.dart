ca9ee379e71c6cb25cf89ec5c61f29285d40142a
Jihyuk Lee <lead@sver.dev>
2025-05-13 15:57:59 +0900

    feat :: 지도 오버레이 구현

--- lib/view/tour/tour.dart
+++ lib/view/tour/tour.dart
@@ -16,6 +16,7 @@ class _TourPageState extends State<TourPage> {
   final mapControllerCompleter = Completer<NaverMapController>();
   final mapViewModel = MapViewmodel();
   NaverMapController? _mapController;
+  final List<NOverlay> _overlays = [];
 
   @override
   void initState() {
@@ -77,6 +78,32 @@ class _TourPageState extends State<TourPage> {
     }
   }
 
+  // 예시 좌표 리스트로 폴리라인 그리기
+  void _drawPolyline() {
+    if (_mapController == null) return;
+
+    // 예시 좌표 리스트
+    final List<NLatLng> coordinates = [
+      const NLatLng(37.5665, 126.9780), // 서울시청
+      const NLatLng(37.5796, 126.9770), // 경복궁
+      const NLatLng(37.5715, 126.9764), // 덕수궁
+      const NLatLng(37.5665, 126.9780), // 서울시청 (닫기)
+    ];
+
+    // 폴리라인 생성
+    final polyline = NPolygonOverlay(
+      id: 'polyline1',
+      coords: coordinates,
+      color: const Color(0x20125CED),
+      outlineColor: const Color(0xFF125CED),
+      outlineWidth: 3,
+    );
+
+    // 오버레이 추가
+    _overlays.add(polyline);
+    _mapController!.addOverlay(polyline);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -90,6 +117,7 @@ class _TourPageState extends State<TourPage> {
           _mapController = controller;
           mapControllerCompleter.complete(controller);
           await _initializeLocation();
+          _drawPolyline();
         },
       ),
     );
